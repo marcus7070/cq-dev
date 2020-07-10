6bc791577a718b6f87e56893ed8f21b14f90fe88
Marcus Boyd <mwb@geosol.com.au>
2020-07-10 11:20:27 +0930

    update to latest CQ master

--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,5 @@
 self: super: {
-  python3 = super.python3.override {
+  python37 = super.python37.override {
     packageOverrides = python-self: python-super: {
       cadquery = python-super.callPackage ./cadquery.nix { documentation = false; };
       ocp = python-super.callPackage ./OCP { };
@@ -8,7 +8,7 @@ self: super: {
       ezdxf = python-super.callPackage ./ezdxf { };
     };
   };
-  cq-editor = super.libsForQt5.callPackage ./cq-editor.nix { };
+  cq-editor = super.libsForQt5.callPackage ./cq-editor.nix { python3Packages = self.python37Packages; };
   # looks like the current release of OCP uses 7.4.0, not the most recent 7.4.0p1 release
   opencascade-occt = super.callPackage ./opencascade-occt/7_4_0.nix { };
 }
